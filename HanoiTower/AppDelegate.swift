40daad7a97b766cb655127290e0e9b8b4603ee92
Spoa <justinpu02@yahoo.com>
2018-01-25 22:45:15 +0800

    Initial Commit

---
+++ HanoiTower/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HanoiTower
+//
+//  Created by Tengoku no Spoa on 2018/1/25.
+//  Copyright © 2018年 Tengoku no Spoa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HanoiTower/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HanoiTower
+//
+//  Created by Tengoku no Spoa on 2018/1/25.
+//  Copyright © 2018年 Tengoku no Spoa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
